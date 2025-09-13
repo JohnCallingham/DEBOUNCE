4be7ab38e598d0aa0e51424c1fd1f00a036fd4a2
JohnCallingham <JohnCallingham@users.noreply.github.com>
2025-09-13 13:23:40 +0100

    Updated code

--- src/Debounce.cpp
+++ src/Debounce.cpp
@@ -1,1 +1,92 @@
+#include "Debounce.h"
 
+Debounce::Debounce(uint8_t inputPin, uint16_t sampleTimemS, uint16_t lowSamples, uint16_t highSamples) {
+  this->inputPin = inputPin;
+  this->sampleTimemS = sampleTimemS;
+  this->lowSamples = lowSamples;
+  this->highSamples = highSamples;
+}
+
+void Debounce::loop() {
+  counter++;
+
+  // Implement a non blocking delay for sampleTimemS.
+  if (millis() < nextUpdate) return;
+  nextUpdate = millis() + sampleTimemS;
+
+  // sampleTimemS has passed since the last call to sample().
+  sample();
+}
+
+void Debounce::sample() {
+
+  // Serial.printf("\n%d in sample(), counter = %d", millis(), counter);
+
+  counter = 0;
+
+  /**
+   * Take a sample of the state of inputPin and then run the debounce process.
+   */
+
+  this->inputState = digitalRead(this->inputPin);
+
+  this->outputState = process(this->inputState);
+}
+
+int Debounce::process(int inputState) {
+  /**
+   * If the high timer is running, decrement it.
+   * If the high timer has counted down to 0,
+   *  and the input is still high, set the output high.
+   */
+  if (highTimer > 0) {
+    highTimer--;
+    if ((highTimer == 0) && (inputState == 1)) {outputState = 1;}
+  }
+
+  /**
+   * If the low timer is running, decrement it.
+   * If the low timer has counted down to 0,
+   *  and the input is still low, set the output low.
+   */
+  if (lowTimer > 0) {
+    lowTimer--;
+    if ((lowTimer == 0) && (inputState == 0)) {outputState = 0;}
+  }
+
+  /**
+   * If the high timer is not running,
+   *  and the output state is low,
+   *  and the input state goes high,
+   *  start the high timer.
+   */
+  if ((highTimer == 0) && (outputState == 0) && (inputState == 1)) {
+    highTimer = highSamples;
+  }
+
+  /**
+   * If the low timer is not running,
+   *  and the output state is high,
+   *  and the input state goes low,
+   *  start the low timer.
+   */
+  if ((lowTimer == 0) && (outputState == 1) && (inputState == 0)) {
+    lowTimer = lowSamples;
+  }
+
+  /**
+   * If there is a transition and either timer is running,
+   *  restart that timer.
+   */
+  if (inputState != lastSampledState) {
+    if (highTimer > 0) {highTimer = highSamples;}
+    if (lowTimer > 0) {lowTimer = lowSamples;}
+  }
+
+  /**
+   * Store this sample's input state for the next sample.
+   */
+  lastSampledState = inputState;
+
+  return outputState;
+}
--- src/Debounce.h
+++ src/Debounce.h
@@ -1,1 +1,62 @@
+#ifndef DEBOUNCE_H
+#define DEBOUNCE_H
 
+#include <Arduino.h>
+
+#define DEBOUNCE_DELAY_mS 100 // 100 mS debounce delay.
+
+/***
+ * Class Debounce provides general debounce functionality.
+ * The input is by a specified input pin.
+ * ??? The output is by a callback function. ???
+ */
+class Debounce {
+  public:
+    /***
+     * inputPin is the pin which is to be debounced.
+     * sampleTimemS is the number of mS between each sample of inputPin.
+     * lowSamples is the number of samples that inputPin needs to be permanently low before a low is output.
+     * highSamples is the number of samples that inputPin needs to be permanently high before a high is output.
+     */
+    Debounce(uint8_t inputPin, uint16_t sampleTimemS, uint16_t lowSamples, uint16_t highSamples);
+
+    /**
+     * Called repeatedly from the main program loop.
+     * Calls sample() when it is time to sample the inputPin.
+     */
+    void loop();
+
+    /**
+     * The state after the bouncing has been removed.
+     * Public so it can be read from the main program.
+     */
+    int outputState;
+
+    private:
+    /**
+     * Called every sampleTimemS to read the state of the input pin and determine if any output change has occurred.
+     */
+    void sample();
+
+    /**
+     * Is passed the state of the input pin and returns the output state.
+     */
+    int process(int inputState);
+
+    int inputState; // The state of the input pin which is bouncing. 0 is low, 1 is high.
+    int lastSampledState; // Used to detect if the input pin has changed state since the last sample. 0 is low, 1 is high.
+
+    unsigned long nextUpdate = 0; // Used to ensure that sample() is called every sampleTimemS;
+
+    uint8_t inputPin; // The pin which is to be debounced.
+    uint16_t sampleTimemS; // The number of mS between each sample of inputPin.
+    uint16_t lowSamples; // The number of samples that inputPin needs to be permanently low before a low is output.
+    uint16_t highSamples; // The number of samples that inputPin needs to be permanently high before a high is output.
+
+    unsigned long counter = 0;  // Counts the number of times that process() is called before sample() is called.
+
+    uint16_t lowTimer = 0; // Counts down from lowSamples. When at zero, the output is set low.
+    uint16_t highTimer = 0; // Counts down from highSamples. When at zero, the output is set high.
+};
+
+#endif
